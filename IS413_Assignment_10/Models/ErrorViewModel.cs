f6cb84cb24fe2410a112da8f1bce6a5b95b3819f
Anthony Sharp <asharp343@gmail.com>
2021-03-29 15:20:49 -0600

    initial

---
+++ IS413_Assignment_10/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IS413_Assignment_10.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
